4ac7a7bf5b824ff0183d35a5851221eee81a21a8
Halla-S <86550752+Halla-S@users.noreply.github.com>
2021-11-18 16:34:41 +0000

    adding more list items

--- src/JobAds.js
+++ src/JobAds.js
@@ -3,31 +3,35 @@ import "./App.css";
 // import Data from "./data.json";
 const JobAds = (props) => {
   return (
-    <div  className="myDiv">
-      {props.data.map((element,index) => {
+    <div className="myDiv">
+      {props.data.map((element, index) => {
         return (
-          <div>
+          <div className="cardDiv">
             <section className="sectionOne">
-              <img src={element.logo} alt=""></img>
+              <img src={element.logo} alt=" " />
             </section>
             <section className="sectionTwo">
-              <h4>{element.company}</h4>
+              <ul>
+                <li key={1}>{element.company}</li>
+                <li key={2} className="new">{element.new === true ? "new" : ""}</li>
+                <li key={3} className="featured">{element.featured === true ? "featured" : ""}</li>
+              </ul>
               <h2>{element.position}</h2>
               <ul>
                 <li key={index}>{element.postedAt}</li>
-                <li key={1+index}>{element.contract}</li>
-                <li key={2+index}>{element.location}</li>
+                <li key={1 + index}>{element.contract}</li>
+                <li key={2 + index}>{element.location}</li>
               </ul>
             </section>
             <section className="sectionThree">
               <ul>
                 <li key={index}>{element.role}</li>
-                <li key={1+ index}>{element.level}</li>
+                <li key={1 + index}>{element.level}</li>
                 {element.languages.map((lan) => {
-                  return <li >{lan}</li>;
+                  return <li>{lan}</li>;
                 })}
                 {element.tools.map((tool) => {
-                  return <li >{tool}</li>;
+                  return <li>{tool}</li>;
                 })}
               </ul>
             </section>
